a9a4634bd682c2c6120e10b50458f9979d6e7e68
Austin Griffith <austin@ethereum.org>
2024-06-20 09:35:39 -0600

    forking rpc, networks, and header

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -42,7 +42,7 @@ const config: HardhatUserConfig = {
     // If the network you are looking for is not here you can add new network settings
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+        url: `https://gnosis.drpc.org`,
         enabled: process.env.MAINNET_FORKING_ENABLED === "true",
       },
     },
